4cc9a95611ab0349ad40795d4ac3ae54c2a73033
Augusto Maza <debianmaza@gmail.com>
2020-07-12 20:45:57 -0300

    Update general

--- index.php
+++ index.php
@@ -602,7 +602,7 @@
   <a href="#myPage" title="To Top">
     <span class="glyphicon glyphicon-chevron-up"></span>
   </a>
-  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
+  <p><strong>Desarrollo:</strong> Slackzone Development - <strong>Email:</strong> develslack@gmail.com</p>
 </footer>
 
 <script>
